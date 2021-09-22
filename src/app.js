1fd51462c21534a0888de5c2af96601290d47254
paola9896 <paolasc2652@gmail.com>
2021-09-22 00:39:53 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  myFunction();
 };
+
+function myFunction() {
+  let suits = ["&hearts;", "&spades;", "&clubs;", "&diams;"];
+  let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  let randomSuit = Math.floor(Math.random() * suits.length);
+  let randomNum = Math.floor(Math.random() * numbers.length);
+
+  let suit = suits[randomSuit];
+  let number = numbers[randomNum];
+
+  if (suit == "&hearts;" || suit == "&diams;") {
+    document.querySelector("#card").style.color = "red";
+  } else {
+    document.querySelector("#card").style.color = "black";
+  }
+
+  document.querySelector("#TopLeft").innerHTML = suit;
+  document.querySelector("#Center").innerHTML = number;
+  document.querySelector("#BottomRight").innerHTML = suit;
+}
